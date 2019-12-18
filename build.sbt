8837db972c9f687ba4392c0e2fa5dc6d7384cb0d
Paschalis Frangidis <pascal.frag@gmail.com>
2019-12-19 00:15:31 +0200

    second commit

--- build.sbt
+++ build.sbt
@@ -3,3 +3,7 @@ name := "RelevanceCalculation"
 version := "0.1"
 
 scalaVersion := "2.11.8"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.4"
+
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.4"
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,5 +1,32 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.{DataFrame, SQLContext, SparkSession}
+
 object main {
 
+    def main(args: Array[String]): Unit = {
+        val ss: SparkSession = SparkSession.builder().config("spark.master", "local[*]").getOrCreate()
+        val sc: SparkContext = ss.sparkContext
+        val sqlContext: SQLContext = ss.sqlContext
 
+        val trainDF: DataFrame = ss.read.format("csv").option("header", "true").load("data/train.csv")
+        val descDF: DataFrame = ss.read.format("csv").option("header", "true").load("data/product_descriptions.csv")
+        val attrDF: DataFrame = ss.read.format("csv").option("header", "true").load("data/attributes.csv")
+
+        /*trainDF.take(20).foreach(x => print(x))
+        descDF.take(20).foreach(x => print(x))
+        attrDF.take(20).foreach(x => print(x))*/
+
+        trainDF.createOrReplaceTempView("train_desc")
+        descDF.createOrReplaceTempView("desc_desc")
+        attrDF.createOrReplaceTempView("attr_desc")
+
+        val trainWithDescAndAttrDF:DataFrame = trainDF.join(descDF, descDF("product_uid") === trainDF("product_uid"))
+        //.join(attrDF, attrDF("product_uid") === trainDF("product_uid"))
+
+        //sqlContext.sql(
+        //    """SELECT a.id, a.product_uid, a.product_title, a.search_term, a.relevance, b.product_description, c.name, c.value FROM train_desc as a INNER JOIN desc_desc as b on a.product_uid = b.product_uid INNER JOIN attr_desc as c on a.product_uid = c.product_uid GROUP BY""")
+
+        trainWithDescAndAttrDF.take(50).foreach(x => print(x))
+    }
 
 }
