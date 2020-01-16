3fa29bb154b5be9cf3afe60d3cbc6d4eb3e8ad72
pasfrag <pascal.frag@gmail.com>
2020-01-17 01:12:28 +0200

    Word2vec, fixed bugs

--- build.sbt
+++ build.sbt
@@ -4,8 +4,6 @@ version := "0.1"
 
 scalaVersion := "2.11.8"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.4"
+libraryDependencies ++= Seq("org.apache.spark" %% "spark-core" % "2.4.4", "org.apache.spark" %% "spark-sql" % "2.4.4", "org.apache.spark" %% "spark-mllib" % "2.4.4")
 
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.4"
-
-libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.4.4"
+libraryDependencies += "com.github.master" % "spark-stemming_2.10" % "0.2.1"
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,7 +1,8 @@
 import org.apache.log4j.{Level, LogManager}
 import org.apache.spark.SparkContext
 import org.apache.spark.ml.feature.{HashingTF, IDF, StopWordsRemover, Tokenizer, Word2Vec}
-import org.apache.spark.sql.{Column, DataFrame, SQLContext, SparkSession}
+import org.apache.spark.mllib.feature.Stemmer
+import org.apache.spark.sql.{Column, DataFrame, SQLContext, SparkSession, functions}
 
 object main {
 
@@ -18,120 +19,136 @@ object main {
         val descDF: DataFrame = spark.read.format("csv").option("header", "true").load("data/product_descriptions.csv")
         val attrDF: DataFrame = spark.read.format("csv").option("header", "true").load("data/attributes.csv")
 
-
-//      -----------------------------Training Data Preprocess-----------------------------------------------
-        // Tokenizing the training data
+        // General used variables
         val tokenizer = new Tokenizer().setInputCol("product_title").setOutputCol("title_tokenized")
-        val semiTokenizedTrainDF = tokenizer.transform(trainDF)
-        tokenizer.setInputCol("search_term").setOutputCol("search_tokenized")
-        val tokenizedTrainDF = tokenizer.transform(semiTokenizedTrainDF)
-        val columnsToRemove = Seq("id", "product_title", "search_term")
-        val removedTrainDF = tokenizedTrainDF.select(tokenizedTrainDF.columns
-          .filter(colName => !columnsToRemove.contains(colName))
-          .map(colName => new Column(colName)): _*
-        )
-
-        //Removing stopwords from training data
         val remover = new StopWordsRemover()
-        remover.setInputCol("title_tokenized").setOutputCol("title_filtered")
-        val semiFilteredTrainDF = remover.transform(removedTrainDF)
-        remover.setInputCol("search_tokenized").setOutputCol("search_filtered")
-        val filteredTrainDF = remover.transform(semiFilteredTrainDF)
-        val finalTrainDF = filteredTrainDF.select(filteredTrainDF.columns
-          .filter(colName => !Seq("title_tokenized", "search_tokenized").contains(colName))
-          .map(colName => new Column(colName)): _*
-        )
-
-        //TODO 2)Lemmatization to training data
-
-        // TF-ing training data
+        val stemmer = new Stemmer().setLanguage("English")
         val hashingTF = new HashingTF()
-        hashingTF.setInputCol("title_filtered").setOutputCol("title_features").setNumFeatures(10000)
-        val semiFeaturesTrainDF = hashingTF.transform(finalTrainDF)
-        hashingTF.setInputCol("search_filtered").setOutputCol("search_features").setNumFeatures(10000)
-        val featuresTrainDF = hashingTF.transform(semiFeaturesTrainDF)
-
-        // TF-IDF training data
         val idf = new IDF()
-        idf.setInputCol("title_features").setOutputCol("title")
-        val semiTrainIDFModel = idf.fit(featuresTrainDF)
-        val semiRescaledTRAINDF = semiTrainIDFModel.transform(featuresTrainDF)
-        idf.setInputCol("search_features").setOutputCol("search")
-        val trainIDFModel = idf.fit(semiRescaledTRAINDF)
-        val rescaledTrainDF = trainIDFModel.transform(semiRescaledTRAINDF)
-        val trainTFIDF = rescaledTrainDF.select(rescaledTrainDF.columns
-          .filter(colName => !Seq("title_filtered", "title_features", "search_filtered", "search_features").contains(colName))
-          .map(colName => new Column(colName)): _*
-        )
+        val word2Vec = new Word2Vec()
+
+
+////      -----------------------------Training Data Preprocess-----------------------------------------------
+//        // Tokenizing the training data
+//        val semiTokenizedTrainDF = tokenizer.transform(trainDF)
+//        tokenizer.setInputCol("search_term").setOutputCol("search_tokenized")
+//        val tokenizedTrainDF = tokenizer.transform(semiTokenizedTrainDF)
+//        val columnsToRemove = Seq("id", "product_title", "search_term")
+//        val removedTrainDF = tokenizedTrainDF.select(tokenizedTrainDF.columns
+//          .filter(colName => !columnsToRemove.contains(colName))
+//          .map(colName => new Column(colName)): _*
+//        )
+//
+//        //Removing stopwords from training data
+//        remover.setInputCol("title_tokenized").setOutputCol("title_filtered")
+//        val semiFilteredTrainDF = remover.transform(removedTrainDF)
+//        remover.setInputCol("search_tokenized").setOutputCol("search_filtered")
+//        val filteredTrainDF = remover.transform(semiFilteredTrainDF)
+//        val finalTrainDF = filteredTrainDF.select(filteredTrainDF.columns
+//          .filter(colName => !Seq("title_tokenized", "search_tokenized").contains(colName))
+//          .map(colName => new Column(colName)): _*
+//        )
+//
+//        // Stemming Training data
+//        val semiStemmedTrainDF = stemmer.setInputCol("title_filtered").setOutputCol("title_stemmed").transform(finalTrainDF)
+//        val stemmedTrainDF = stemmer.setInputCol("search_filtered").setOutputCol("search_stemmed").transform(semiStemmedTrainDF)
+//
+//        // TF-ing training data
+//        hashingTF.setInputCol("title_stemmed").setOutputCol("title_features").setNumFeatures(10000)
+//        val semiFeaturesTrainDF = hashingTF.transform(stemmedTrainDF)
+//        hashingTF.setInputCol("search_stemmed").setOutputCol("search_features").setNumFeatures(10000)
+//        val featuresTrainDF = hashingTF.transform(semiFeaturesTrainDF)
+//
+//        // TF-IDF training data
+//        idf.setInputCol("title_features").setOutputCol("title")
+//        val semiTrainIDFModel = idf.fit(featuresTrainDF)
+//        val semiRescaledTRAINDF = semiTrainIDFModel.transform(featuresTrainDF)
+//        idf.setInputCol("search_features").setOutputCol("search")
+//        val trainIDFModel = idf.fit(semiRescaledTRAINDF)
+//        val rescaledTrainDF = trainIDFModel.transform(semiRescaledTRAINDF)
+//        val trainTFIDF = rescaledTrainDF.select(rescaledTrainDF.columns
+//          .filter(colName => !Seq("title_filtered", "title_features", "title_stemmed", "search_filtered", "search_features", "search_stemmed").contains(colName))
+//          .map(colName => new Column(colName)): _*
+//        )
 //        trainTFIDF.printSchema()
 //        trainTFIDF.take(10).foreach(x => print(x))
-
-        // Word2Vec to training data
-        val word2Vec = new Word2Vec()
-        word2Vec.setInputCol("title_filtered").setOutputCol("title").setMinCount(0)//.setVectorSize(50)
-        val semiTrainVecModel = word2Vec.fit(finalTrainDF)
-        val semiVecTrainDF = semiTrainVecModel.transform(finalTrainDF)
-        word2Vec.setInputCol("search_filtered").setOutputCol("search").setMinCount(0)//.setVectorSize(50)
-        val trainVecModel = word2Vec.fit(semiVecTrainDF)
-        val vecTrainDF = trainVecModel.transform(semiVecTrainDF)
-        val trainW2VDF = vecTrainDF.select(vecTrainDF.columns
-          .filter(colName => !Seq("title_filtered", "search_filtered").contains(colName))
-          .map(colName => new Column(colName)): _*
-        )
+//
+//        // Word2Vec to training data
+//        word2Vec.setInputCol("title_stemmed").setOutputCol("title")
+//        val semiTrainVecModel = word2Vec.fit(stemmedTrainDF)
+//        val semiVecTrainDF = semiTrainVecModel.transform(stemmedTrainDF)
+//        word2Vec.setInputCol("search_stemmed").setOutputCol("search")
+//        val trainVecModel = word2Vec.fit(semiVecTrainDF)
+//        val vecTrainDF = trainVecModel.transform(semiVecTrainDF)
+//        val trainW2VDF = vecTrainDF.select(vecTrainDF.columns
+//          .filter(colName => !Seq("title_filtered", "title_stemmed", "search_filtered", "search_stemmed").contains(colName))
+//          .map(colName => new Column(colName)): _*
+//        )
 //        trainW2VDF.printSchema()
 //        trainW2VDF.take(10).foreach(x => print(x))
-
+//
 
 //      -----------------------------Description Data Preprocess-----------------------------------------------
-        // Tokenizing the description data
-        tokenizer.setInputCol("product_description").setOutputCol("description_tokenized")
-        val tokenizedDescriptionDF = tokenizer.transform(descDF)
-        val removedDescriptionDF = tokenizedDescriptionDF.select(tokenizedDescriptionDF.columns
-          .filter(colName => colName != "product_description")
-          .map(colName => new Column(colName)): _*
-        )
-
-        // Removing stopwords from description data
-        remover.setInputCol("description_tokenized").setOutputCol("description_filtered")
-        val filteredDescriptionDF = remover.transform(removedDescriptionDF)
-        val finalDescriptionDF = filteredDescriptionDF.select(filteredDescriptionDF.columns
-            .filter(colName => colName != "description_tokenized")
-            .map(colName => new Column(colName)): _*
-        )
-
-        //TODO 3)Lemmatization to description data
-
-        // TF-ing description data
-        hashingTF.setInputCol("description_filtered").setOutputCol("description_features").setNumFeatures(10000)
-        val featuresDescriptionDF = hashingTF.transform(finalDescriptionDF)
-
-        // TF-IDF description data
-        idf.setInputCol("description_features").setOutputCol("description")
-        val descriptionIDFModel = idf.fit(featuresDescriptionDF)
-        val rescaledDescriptionDF = descriptionIDFModel.transform(featuresDescriptionDF)
-        val descriptionTFIDF = rescaledDescriptionDF.select(rescaledDescriptionDF.columns
-          .filter(colName => !Seq("description_filtered", "description_features").contains(colName))
-          .map(colName => new Column(colName)): _*
-        )
+//        // Tokenizing the description data
+//        tokenizer.setInputCol("product_description").setOutputCol("description_tokenized")
+//        val tokenizedDescriptionDF = tokenizer.transform(descDF)
+//        val removedDescriptionDF = tokenizedDescriptionDF.select(tokenizedDescriptionDF.columns
+//          .filter(colName => colName != "product_description")
+//          .map(colName => new Column(colName)): _*
+//        )
+//
+//        // Removing stopwords from description data
+//        remover.setInputCol("description_tokenized").setOutputCol("description_filtered")
+//        val filteredDescriptionDF = remover.transform(removedDescriptionDF)
+//        val finalDescriptionDF = filteredDescriptionDF.select(filteredDescriptionDF.columns
+//            .filter(colName => colName != "description_tokenized")
+//            .map(colName => new Column(colName)): _*
+//        )
+//
+//        // Stemming description data
+//        val stemmedDescriptionDF = stemmer.setInputCol("description_filtered").setOutputCol("description_stemmed").transform(finalDescriptionDF)
+//
+//        // TF-ing description data
+//        hashingTF.setInputCol("description_stemmed").setOutputCol("description_features").setNumFeatures(10000)
+//        val featuresDescriptionDF = hashingTF.transform(stemmedDescriptionDF)
+//
+//        // TF-IDF description data
+//        idf.setInputCol("description_features").setOutputCol("description")
+//        val descriptionIDFModel = idf.fit(featuresDescriptionDF)
+//        val rescaledDescriptionDF = descriptionIDFModel.transform(featuresDescriptionDF)
+//        val descriptionTFIDF = rescaledDescriptionDF.select(rescaledDescriptionDF.columns
+//          .filter(colName => !Seq("description_filtered", "description_stemmed", "description_features").contains(colName))
+//          .map(colName => new Column(colName)): _*
+//        )
 //        descriptionTFIDF.printSchema()
-//        descriptionTFIDF.take(10).foreach(x => print(x))
+//        descriptionTFIDF.take(1).foreach(x => print(x))
+//
+//        // Word2Vec to description data
+//        word2Vec.setInputCol("description_stemmed").setOutputCol("description")
+//        val descVecModel = word2Vec.fit(stemmedDescriptionDF)
+//        val vecDescDF = descVecModel.transform(stemmedDescriptionDF)
+//        val descriptionW2VDF = vecDescDF.select(vecDescDF.columns
+//          .filter(colName => !Seq("description_filtered", "description_stemmed").contains(colName))
+//          .map(colName => new Column(colName)): _*
+//        )
+//        descriptionW2VDF.printSchema()
+//        descriptionW2VDF.take(10).foreach(x => print(x))
 
-        // Word2Vec to description data
-        word2Vec.setInputCol("description_filtered").setOutputCol("description").setMinCount(0).setVectorSize(50)
-        val descVecModel = word2Vec.fit(finalDescriptionDF)
-        val vecDescDF = descVecModel.transform(finalDescriptionDF)
-        val descriptionW2VDF = vecDescDF.select(vecDescDF.columns
-          .filter(colName => !Seq("description_filtered").contains(colName))
-          .map(colName => new Column(colName)): _*
-        )
-        descriptionW2VDF.printSchema()
-        descriptionW2VDF.take(10).foreach(x => print(x))
 
+//-----------------------------Attribute Data Preprocess-----------------------------------------------
 
-//      -----------------------------Attribute Data Preprocess-----------------------------------------------
+        val result1 = attrDF.groupBy("product_uid").agg(functions.collect_set("name"))
+        val result2 = attrDF.groupBy("product_uid").agg(functions.collect_set("value"))
+        val result3 = result1.join(result2, result2("product_uid") === result1("product_uid"), "inner")
+          .select(result1("product_uid"), result1("collect_set(name)"), result2("collect_set(value)"))
+        val result4 = result3
+          .withColumn("name", functions.concat_ws(",", result3("collect_set(name)")))
+          .withColumn("value", functions.concat_ws(",", result3("collect_set(value)")))
+          .orderBy("product_uid").select("product_uid", "name", "value")
+
         // Tokenizing the attribute data
         tokenizer.setInputCol("name").setOutputCol("name_tokenized")
-        val semiTokenizedAttributeDF = tokenizer.transform(attrDF)
+        val semiTokenizedAttributeDF = tokenizer.transform(result4)
         tokenizer.setInputCol("value").setOutputCol("value_tokenized")
         val tokenizedAttributeDF = tokenizer.transform(semiTokenizedAttributeDF)
         val columnsToRemove1 = Seq("name", "value")
@@ -168,18 +185,18 @@ object main {
 //        val attributeIDFModel = idf.fit(semiRescaledAttributeIDF)
 //        val rescaledAttributeDF = attributeIDFModel.transform(semiRescaledAttributeIDF)
 
-//        idf.setInputCol("name_features").setOutputCol("name")
-//        val semiAttributeIDFModel = idf.fit(featuresAttributeDF)
-//        val semiRescaledAttributeDF = semiAttributeIDFModel.transform(featuresAttributeDF)
-//        idf.setInputCol("value_features").setOutputCol("value")
-//        val attributeIDFModel = idf.fit(semiRescaledAttributeDF)
-//        val rescaledAttributeDF = attributeIDFModel.transform(semiRescaledAttributeDF)
-//        val attributeTFIDF = rescaledAttributeDF.select(rescaledAttributeDF.columns
-//          .filter(colName => !Seq("name_filtered", "name_features", "value_filtered", "value_features").contains(colName))
-//          .map(colName => new Column(colName)): _*
-//        )
-//        attributeTFIDF.printSchema()
-//        attributeTFIDF.take(10).foreach(x => print(x))
+        idf.setInputCol("name_features").setOutputCol("name")
+        val semiAttributeIDFModel = idf.fit(featuresAttributeDF)
+        val semiRescaledAttributeDF = semiAttributeIDFModel.transform(featuresAttributeDF)
+        idf.setInputCol("value_features").setOutputCol("value")
+        val attributeIDFModel = idf.fit(semiRescaledAttributeDF)
+        val rescaledAttributeDF = attributeIDFModel.transform(semiRescaledAttributeDF)
+        val attributeTFIDF = rescaledAttributeDF.select(rescaledAttributeDF.columns
+          .filter(colName => !Seq("name_filtered", "name_features", "value_filtered", "value_features").contains(colName))
+          .map(colName => new Column(colName)): _*
+        )
+        attributeTFIDF.printSchema()
+        attributeTFIDF.take(10).foreach(x => print(x))
 
 //        trainDF.take(20).foreach(x => print(x))
 //        descDF.take(20).foreach(x => print(x))
